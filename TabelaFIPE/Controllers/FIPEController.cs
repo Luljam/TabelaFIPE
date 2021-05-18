6a0bd6e3cfe28c5977c9a5b04db1750324296efe
luljam <lucianofdebrito@gmail.com>
2021-05-18 12:27:52 -0300

    Add Comentarios de tratamento de erros em FIPEController

--- TabelaFIPE/Controllers/FIPEController.cs
+++ TabelaFIPE/Controllers/FIPEController.cs
@@ -29,9 +29,11 @@ namespace TabelaFIPE.Controller
         /// <returns>Retorna marcas de veiculos conforme o tipo: Carros, Motos ou Caminhoes</returns>
         /// <response code="200">Returna as marcas</response>
         /// <response code="400">Se a marca null</response>
+        /// <response code="404">Se caso não encontre marca</response>
         [HttpGet("marcas/{tipo}", Name = "GetMarcaVeiculos")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetAllMarcas(string tipo)
         {
             if(tipo == string.Empty)
@@ -50,8 +52,12 @@ namespace TabelaFIPE.Controller
         /// Retorna os veículos da marca passada no parâmento po Id da Marca
         /// </summary>
         /// <param name="idMarca"></param>
-        /// <returns></returns>
+        /// <returns>Retorna os veículos por id da marca</returns>
+        /// <response code="200">Returna veículos por marca</response>
+        /// <response code="404">Se caso não encontre os veículos</response>
         [HttpGet("veiculo/{idMarca}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetVeiculosPorMarca(int idMarca)
         {
             var veiculos = await veiculosServices.GetVeiculosMarca(idMarca);
@@ -67,8 +73,12 @@ namespace TabelaFIPE.Controller
         /// </summary>
         /// <param name="idMarca"></param>
         /// <param name="codigoVeiculo"></param>
-        /// <returns></returns>
+        /// <returns>Retorna o veículo (singular), passando o Id da Marca e do código do veiculo </returns>
+        /// <response code="200">Returna veículo por código</response>
+        /// <response code="404">Se caso não encontre os veículos</response>
         [HttpGet("veiculo/{idMarca}/{codigoVeiculo}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetVeiculo(int idMarca, string codigoVeiculo)
         {
             var veiculo = await veiculosServices.GetVeiculo(idMarca, codigoVeiculo);
@@ -85,8 +95,12 @@ namespace TabelaFIPE.Controller
         /// <param name="idMarca"></param>
         /// <param name="codigoVeiculo"></param>
         /// <param name="ano"></param>
-        /// <returns></returns>
+        /// <returns>Retorna os dados completos de valores do veículo, passando o Id da Marca, o código do veículo e o ano do veículo</returns>
+        /// <response code="200">Returna o veículo</response>
+        /// <response code="404">Se caso não encontre o veículo</response>
         [HttpGet("veiculo/{idMarca}/{codigoVeiculo}/{ano}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetVeiculoAno(int idMarca, string codigoVeiculo, string ano)
         {
             var veiculoAno = await veiculosServices.GetVeiculoAno(idMarca, codigoVeiculo, ano);
